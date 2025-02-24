840c1171d1b4aa1ac6ba00350c77c205952d6ac0
Rydhlnst <rydhlnst@gmail.com>
2025-02-24 09:18:20 +0700

    ACL Middleware

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,7 @@ import mongoose, { mongo } from "mongoose";
 import { encrypt } from "../utils/encryption";
 import {renderMailHtml, sendMail} from "../utils/mail/mail";
 import { CLIENT_HOST, EMAIL_SMPT_USER } from "../utils/env";
+import { ROLES } from "../utils/constant";
 
 export interface IUser {
     fullName: string,
@@ -39,8 +40,8 @@ const UserSchema = new Schema<IUser>({
     },
     role: {
         type: Schema.Types.String,
-        enum: ["admin", "user"],
-        default: "user",
+        enum: [ROLES.ADMIN, ROLES.MEMBER],
+        default: ROLES.MEMBER,
     },
     profilePicture: {
         type: Schema.Types.String,
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import dummyController from "../controller/dummy.controller";
 import authController from "../controller/auth.controller";
 import authMiddleware from "../middleware/auth.middleware";
+import aclMiddleware from "../middleware/acl.middleware";
+import { ROLES } from "../utils/constant";
 
 const router = express.Router();
 
@@ -11,5 +13,12 @@ router.post("/auth/login", authController.login);
 router.get("/auth/me", authMiddleware, authController.me);
 // Jika terdapat error TypeError: Failed to execute 'fetch' on 'Window': Request with GET/HEAD method cannot have body. Masalahnya disini
 router.post("/auth/activation", authController.activation);
+
+router.get("/test-acl", [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])], (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Ok",
+        data: "Success"
+    })
+})
 
 export default router;
